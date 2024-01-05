2de2134bf1428ad9f764d10a406383a5a2fd5e1c
Oğuzhan Öksel <oguzhanoksel@hotmail.com>
2024-01-06 00:18:58 +0300

    Forms

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from blog.views import BlogDetailView, BlogListView
+from blog.views import (
+    BlogDetailView, 
+    BlogListView, 
+    BlogCreateView, 
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
 ]
